a6f7ec31a6ba4e790768fa9ac3b07449709dfd0a
kalp <kalpanabiradar.k@gmail.com>
2024-12-18 14:19:49 +0530

    Changes

---
+++ api.js
@@ -0,0 +1,27 @@
+// console.log("Navbar");
+
+
+// setTimeout(()=>(
+//     console.log("Navbar Data")
+// ),5000)
+
+// console.log("Body");
+
+// setTimeout(()=>{
+//     console.log("Body Data");
+// },2000)
+// console.log("footer");
+
+// console.log("one");
+
+// setInterval(()=>{
+//     console.log("Jai bhajarangi");
+// },2000)
+// console.log("two");
+
+// setInterval(()=>{
+//     console.log("Jai mata di");
+// },2000)
+
+//API--->Application programming interface
+//restful api's---
